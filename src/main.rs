658d963579bbdab0b1502f9fd9e7f6228ea0c554
Buremoh Michael <161516631+Mikkyfrosh05@users.noreply.github.com>
2024-04-17 07:58:38 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use colorful::Colorful;
 fn main() {
     let args : Vec<String> = std::env::args().collect();
 
-    let config = rust_mkdir::Config::build(args.clone()).unwrap_or_else(|err| {
+    let config = rust_mkdir2::Config::build(args.clone()).unwrap_or_else(|err| {
         eprintln!("{}", format!("⚠️ Problem parsing arguments: {}", err).gradient(Color::Red));
         eprintln!("{}", format!("ℹ Try 'mkdir --help' for more information.").gradient(Color::Green));
         std::process::exit(1);
